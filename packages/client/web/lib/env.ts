14af29ab2e7662234ff9070b99b07c92d67afc90
John Beresford <jberesford@volcaus.com>
2024-07-27 15:33:54 -0700

    fix: test client web version

--- packages/client/web/lib/env.ts
+++ packages/client/web/lib/env.ts
@@ -1,5 +1,6 @@
 export const API_HOSTNAME = process.env.RETROM_HOSTNAME ?? "http://localhost";
 export const API_PORT = process.env.RETROM_PORT ?? "5101";
+
 const API_HOST = process.env.RETROM_HOST ?? `${API_HOSTNAME}:${API_PORT}`;
 
 export const GRPC_HOST = process.env.GRPC_HOST ?? API_HOST;
