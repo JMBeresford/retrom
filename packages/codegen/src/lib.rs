b6863a290fbb3e3f28dfe4ebb59f0ee640e9436c
John Beresford <jberesford@volcaus.com>
2024-07-27 13:21:00 -0700

    fix: test deps versioning

--- packages/codegen/src/lib.rs
+++ packages/codegen/src/lib.rs
@@ -6,7 +6,6 @@ pub mod igdb {
 
 pub mod retrom {
     use retrom_db::schema;
-
     tonic::include_proto!("retrom");
 
     pub const FILE_DESCRIPTOR_SET: &[u8] = tonic::include_file_descriptor_set!("retrom_descriptor");
