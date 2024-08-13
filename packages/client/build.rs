d6dd745dd2a1371dd4e73ba9eb585c9ea6a26f82
John Beresford <jberesford@volcaus.com>
2024-08-13 14:39:05 -0700

    fix: windows build psql linking

--- packages/client/build.rs
+++ packages/client/build.rs
@@ -1,3 +1,8 @@
 fn main() {
+    #[cfg(target_os = "windows")]
+    println!("cargo::rustc-link-search=\"C:\\Program Files\\PostgreSQL\\16\\lib\"");
+    #[cfg(target_os = "windows")]
+    println!("cargo:rustc-link-lib=libpq");
+
     tauri_build::build()
 }
