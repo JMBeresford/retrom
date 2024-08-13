66b4e4a7691ad0e8e75fef06ecff8d962bd99a45
John Beresford <jberesford@volcaus.com>
2024-08-13 13:05:40 -0700

    ibid

--- packages/client/web/lib/env.ts
+++ packages/client/web/lib/env.ts
@@ -1,4 +1,6 @@
 export const IS_DESKTOP =
-  window !== undefined
+  typeof window !== "undefined"
     ? "__TAURI__" in window || "__TAURI_INTERNALS__" in window
     : undefined;
+
+console.log({ IS_DESKTOP });
