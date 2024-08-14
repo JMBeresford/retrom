c62eb59c877c254cda5ad409e9d6edc4971fbca3
John Beresford <jberesford@volcaus.com>
2024-08-13 19:07:24 -0700

    fix: macOS adhoc signing

--- packages/client/build.rs
+++ packages/client/build.rs
@@ -1,8 +1,3 @@
 fn main() {
-    #[cfg(target_os = "windows")]
-    println!("cargo::rustc-link-search=\"C:\\Program Files\\PostgreSQL\\16\\lib\"");
-    #[cfg(target_os = "windows")]
-    println!("cargo:rustc-link-lib=libpq");
-
     tauri_build::build()
 }
