9da14d007e40042d16cdd9fbb2fd802de3c1a5e6
John Beresford <jberesford@volcaus.com>
2024-08-13 20:17:38 -0700

    Update build.rs

--- packages/client/build.rs
+++ packages/client/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     #[cfg(target_os = "windows")]
-    println!("cargo::rustc-link-search=\"C:\\Program Files\\PostgreSQL\\16\\lib\"");
+    println!("cargo::rustc-link-search=C:\\Program Files\\PostgreSQL\\16\\lib");
     #[cfg(target_os = "windows")]
     println!("cargo:rustc-link-lib=libpq");
 
