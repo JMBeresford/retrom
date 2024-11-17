2dfea2e3b2def0026f3fa7e933fceb41aa6f008f
John Beresford <jberesford@volcaus.com>
2024-11-16 17:58:31 -0800

    fix: auto-updating

--- packages/client/web/src/queries/useUpdateCheck.ts
+++ packages/client/web/src/queries/useUpdateCheck.ts
@@ -1,20 +1,17 @@
 import { checkIsDesktop } from "@/lib/env";
 import { useQuery } from "@tanstack/react-query";
-import { check, Update } from "@tauri-apps/plugin-updater";
+import { check } from "@tauri-apps/plugin-updater";
 
 export function useUpdateCheck() {
   return useQuery({
     queryKey: ["update-check"],
     queryFn: async () => {
       if (checkIsDesktop()) {
-        check().then((update) => update);
+        const update = await check();
+
+        return update;
       } else {
-        return new Update({
-          rid: -1,
-          version: "0.0.0",
-          available: false,
-          currentVersion: "0.0.0",
-        });
+        return null;
       }
     },
   });
