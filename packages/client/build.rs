f363f622f4f187a6e646afa8462019204bef5d42
John Beresford <jberesford@volcaus.com>
2025-01-07 18:13:50 -0800

    again

--- packages/client/build.rs
+++ packages/client/build.rs
@@ -1,13 +1,3 @@
 fn main() {
-    #[cfg(target_os = "windows")]
-    println!("cargo::rustc-link-search=C:\\Program Files\\PostgreSQL\\17\\lib");
-    #[cfg(target_os = "windows")]
-    println!("cargo:rustc-link-lib=libpq");
-
-    #[cfg(target_os = "macos")]
-    {
-        println!("cargo:rustc-link-search=$DYLD_FALLBACK_LIBRARY_PATH");
-    }
-
     tauri_build::build()
 }
