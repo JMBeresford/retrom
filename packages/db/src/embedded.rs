129b3652f597179a349c14085657a125e1b57965
John Beresford <jberesford@volcaus.com>
2025-01-31 18:22:23 -0800

    chore: update psql-embedded deps

--- packages/db/src/embedded.rs
+++ packages/db/src/embedded.rs
@@ -22,22 +22,16 @@ pub async fn start_embedded_db(url: &str) -> crate::Result<impl PgCtlFailsafeOpe
 
     let mut psql = PostgreSQL::new(settings);
     if let Err(err) = psql.setup().await {
-        use postgresql_commands::Error as CommandError;
         use postgresql_embedded::Error as EmbeddedError;
 
         match &err {
-            EmbeddedError::DatabaseInitializationError(some_err) => {
-                match some_err.downcast_ref::<CommandError>() {
-                    Some(CommandError::TimeoutError(_)) => {
-                        let status = psql.status();
+            EmbeddedError::DatabaseInitializationError(_) => {
+                let status = psql.status();
 
-                        tracing::warn!(
-                            "Could not confirm embedded DB has initialized, current status: {:?}",
-                            status
-                        );
-                    }
-                    _ => return Err(crate::Error::EmbeddedError(err)),
-                }
+                tracing::warn!(
+                    "Could not confirm embedded DB has initialized, current status: {:?}",
+                    status
+                );
             }
             _ => return Err(crate::Error::EmbeddedError(err)),
         }
