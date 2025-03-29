951d5faa627dcbaf95242f978dd865cf5a911665
John Beresford <jberesford@volcaus.com>
2025-03-29 15:09:51 -0700

    chore: clean up

--- packages/client/web/src/components/fullscreen/focus-container/index.tsx
+++ packages/client/web/src/components/fullscreen/focus-container/index.tsx
@@ -88,7 +88,6 @@ function FocusableElementImpl<T extends HTMLElement>(
 
   const onClickHandler = useCallback(
     (e: MouseEvent<T>) => {
-      console.log("CLICKHANDLER: ", e);
       focusSelf();
       childProps.onClick?.(e);
       onClick?.(e);
