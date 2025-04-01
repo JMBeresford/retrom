37606d55239e698f230ae836d2648d9a06191847
John Beresford <jberesford@volcaus.com>
2025-03-31 19:44:49 -0700

    etc

--- plugins/retrom-plugin-service-client/guest-js/index.ts
+++ plugins/retrom-plugin-service-client/guest-js/index.ts
@@ -1,9 +1,12 @@
-import { invoke } from '@tauri-apps/api/core'
+import { invoke } from "@tauri-apps/api/core";
 
 export async function ping(value: string): Promise<string | null> {
-  return await invoke<{value?: string}>('plugin:retrom-plugin-service-client|ping', {
-    payload: {
-      value,
+  return await invoke<{ value?: string }>(
+    "plugin:retrom-plugin-service-client|ping",
+    {
+      payload: {
+        value,
+      },
     },
-  }).then((r) => (r.value ? r.value : null));
+  ).then((r) => (r.value ? r.value : null));
 }
