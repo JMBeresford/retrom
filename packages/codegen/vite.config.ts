e3aa85c06e892f7381db2872f05072d956370ade
John Beresford <jberesford@volcaus.com>
2025-07-03 12:33:44 -0700

    thanks windows

--- packages/codegen/vite.config.ts
+++ packages/codegen/vite.config.ts
@@ -3,11 +3,8 @@ import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
 import { nxCopyAssetsPlugin } from "@nx/vite/plugins/nx-copy-assets.plugin";
-import { fileURLToPath } from "node:url";
-import { dirname, resolve, relative, extname } from "node:path";
+import { resolve, relative, extname } from "node:path";
 import { globSync } from "glob";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(() => ({
   root: __dirname,
@@ -20,7 +17,7 @@ export default defineConfig(() => ({
     }),
   ],
   build: {
-    outDir: resolve(__dirname, "./dist"),
+    outDir: "./dist",
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
