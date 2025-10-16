e9eb71f6aa2421a012d072a61ace3b932308468f
John Beresford <jberesford@volcaus.com>
2025-10-15 19:43:34 -0700

    fix: sidebar load times

---
+++ packages/client-web/src/components/side-bar/game-list.tsx
@@ -0,0 +1,214 @@
+import { getFileStub } from "@/lib/utils";
+import {
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@retrom/ui/components/accordion";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@retrom/ui/components/dropdown-menu";
+import { cn } from "@retrom/ui/lib/utils";
+import { ComponentPropsWithoutRef, useMemo } from "react";
+import { PlatformWithMetadata } from ".";
+import { useGames } from "@/queries/useGames";
+import { Skeleton } from "@retrom/ui/components/skeleton";
+import { GameItem } from "./game-item";
+import { Button } from "@retrom/ui/components/button";
+import { EllipsisVertical } from "lucide-react";
+import { Link } from "@tanstack/react-router";
+import { useFilterAndSort } from "./filter-sort-context";
+import { filterName, sortGames } from "./utils";
+import { useInstallationIndex } from "@/providers/installation-index";
+import { InstallationStatus } from "@retrom/codegen/retrom/client/installation_pb";
+
+export function GameList(props: { platform: PlatformWithMetadata }) {
+  const { platform } = props;
+
+  const { gameSortKey, filters, gameSortDirection, groupByInstallationStatus } =
+    useFilterAndSort();
+  const { installations } = useInstallationIndex();
+
+  const name = platform.metadata?.name || getFileStub(platform.path);
+
+  const { data: allGames, status: gamesStatus } = useGames({
+    request: { platformIds: [platform.id], withMetadata: true },
+    selectFn: (data) => data.games,
+  });
+
+  const games = useMemo(() => {
+    const games =
+      allGames?.filter((game) => filterName(game, filters.name)) ?? [];
+
+    games.sort((a, b) => sortGames(a, b, gameSortKey, gameSortDirection));
+
+    if (groupByInstallationStatus) {
+      games.sort((a, b) => {
+        const aInstalled = installations[a.id] === InstallationStatus.INSTALLED;
+
+        const bInstalled = installations[b.id] === InstallationStatus.INSTALLED;
+
+        if (aInstalled && !bInstalled) {
+          return -1;
+        }
+
+        if (!aInstalled && bInstalled) {
+          return 1;
+        }
+
+        return 0;
+      });
+    }
+
+    console.log({ name, games, allGames });
+
+    return games;
+  }, [
+    name,
+    allGames,
+    filters.name,
+    gameSortDirection,
+    gameSortKey,
+    groupByInstallationStatus,
+    installations,
+  ]);
+
+  if (gamesStatus === "pending") {
+    return <Skeleton className="w-full h-12" />;
+  }
+
+  if (gamesStatus === "error") {
+    return (
+      <div className="grid place-items-center pt-4">
+        <p className="font-medium text-destructive-text italic">
+          Failed to load games.
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <AccordionItem
+      key={platform.id}
+      value={platform.id.toString()}
+      className={cn("border-b-0 w-full max-w-full")}
+    >
+      <div
+        className={cn(
+          "group grid grid-cols-[1fr_auto] border-b border-transparent",
+          "sm:hover:border-border transition-all",
+        )}
+      >
+        <AccordionTrigger
+          hideIcon
+          className={cn(
+            "group py-2 font-medium overflow-hidden relative hover:no-underline",
+          )}
+        >
+          <div className="flex w-full">
+            <h3 className="text-left text-lg sm:text-base whitespace-nowrap text-ellipsis overflow-hidden">
+              {name}
+            </h3>
+            <span className="sr-only">Toggle</span>
+          </div>
+        </AccordionTrigger>
+
+        <PlatformContextMenu platform={platform} />
+      </div>
+
+      <AccordionContent>
+        {games.length ? (
+          <ul>
+            {games.map((game) => {
+              return <GameItem key={game.id} game={game} />;
+            })}
+          </ul>
+        ) : (
+          <div className="grid place-items-center pt-4">
+            <p className="font-medium text-muted-foreground italic">
+              Theres no games here...
+            </p>
+          </div>
+        )}
+      </AccordionContent>
+    </AccordionItem>
+  );
+}
+
+function PlatformContextMenu(
+  props: ComponentPropsWithoutRef<typeof DropdownMenuTrigger> & {
+    platform: PlatformWithMetadata;
+  },
+) {
+  const { platform, ...rest } = props;
+
+  const name = platform.metadata?.name || getFileStub(platform.path);
+  const { id, thirdParty } = platform;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger
+        asChild
+        {...rest}
+        className={cn(
+          "sm:opacity-0 transition-opacity active:opacity-100",
+          "sm:group-hover:opacity-100 data-[state=open]:opacity-100",
+        )}
+      >
+        <Button
+          size="icon"
+          variant="ghost"
+          className="w-fit h-fit aspect-square p-2 my-auto"
+        >
+          <EllipsisVertical className={cn("w-[1rem] h-[1rem]")} />
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent
+        className={cn(
+          "w-dvw flex flex-col items-center gap-2",
+          "sm:w-auto sm:block",
+        )}
+      >
+        <DropdownMenuItem
+          asChild
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+        >
+          <Link
+            to="."
+            search={(prev) => ({
+              ...prev,
+              updatePlatformMetadataModal: { open: true, id: platform.id },
+            })}
+          >
+            Edit
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          asChild
+          className="text-destructive-text"
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+        >
+          <Link
+            to="."
+            search={(prev) => ({
+              ...prev,
+              deletePlatformModal: {
+                open: true,
+                platform: { id, name, thirdParty },
+              },
+            })}
+          >
+            Delete
+          </Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
--- packages/client-web/src/components/side-bar/index.tsx
+++ packages/client-web/src/components/side-bar/index.tsx
@@ -1,113 +1,39 @@
-import { Image, getFileName, getFileStub } from "@/lib/utils";
-import { cn } from "@retrom/ui/lib/utils";
-import { ComponentPropsWithoutRef, useMemo } from "react";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@retrom/ui/components/tooltip";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@retrom/ui/components/accordion";
-import { usePlatforms } from "@/queries/usePlatforms";
 import { useGames } from "@/queries/useGames";
-import { Game } from "@retrom/codegen/retrom/models/games_pb";
-import {
-  GameMetadata,
-  PlatformMetadata,
-} from "@retrom/codegen/retrom/models/metadata_pb";
-import { Link, useLocation } from "@tanstack/react-router";
+import { usePlatforms } from "@/queries/usePlatforms";
+import { PlatformMetadata } from "@retrom/codegen/retrom/models/metadata_pb";
+import { Platform } from "@retrom/codegen/retrom/models/platforms_pb";
+import { Accordion } from "@retrom/ui/components/accordion";
+import { ScrollArea } from "@retrom/ui/components/scroll-area";
+import { Separator } from "@retrom/ui/components/separator";
+import { Skeleton } from "@retrom/ui/components/skeleton";
+import { TooltipProvider } from "@retrom/ui/components/tooltip";
+import { cn } from "@retrom/ui/lib/utils";
+import { Link, useParams } from "@tanstack/react-router";
+import { useMemo } from "react";
 import { useFilterAndSort } from "./filter-sort-context";
 import { FiltersAndSorting } from "./filters-and-sorting";
-import { Separator } from "@retrom/ui/components/separator";
-import { filterName, sortGames, sortPlatforms } from "./utils";
-import { ScrollArea } from "@retrom/ui/components/scroll-area";
-import { TooltipPortal } from "@retrom/ui/components/tooltip";
-import { InstallationStatus } from "@retrom/codegen/retrom/client/installation_pb";
-import { Skeleton } from "@retrom/ui/components/skeleton";
-import { EllipsisVertical } from "lucide-react";
-import { Platform } from "@retrom/codegen/retrom/models/platforms_pb";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@retrom/ui/components/dropdown-menu";
-import { Button } from "@retrom/ui/components/button";
-import { StorageType } from "@retrom/codegen/retrom/server/config_pb";
-import { useGameMetadata } from "@/queries/useGameMetadata";
-import { createUrl, usePublicUrl } from "@/utils/urls";
-import { GetGameMetadataResponse_MediaPaths } from "@retrom/codegen/retrom/services/metadata-service_pb";
-import { useInstallationIndex } from "@/providers/installation-index";
-import { useInstallationStatus } from "@/queries/useInstallationStatus";
-import { useInstallationProgress } from "@/queries/useInstallationProgress";
+import { GameList } from "./game-list";
+import { sortPlatforms } from "./utils";
 
-type PlatformWithMetadata = Platform & { metadata?: PlatformMetadata };
-type GameMetadataWithMediaPaths = {
-  metadata: GameMetadata;
-  mediaPaths?: GetGameMetadataResponse_MediaPaths;
-};
+export type PlatformWithMetadata = Platform & { metadata?: PlatformMetadata };
 
 export function SideBar() {
-  const {
-    gameSortKey,
-    filters,
-    gameSortDirection,
-    platformSortKey,
-    platformSortDirection,
-    groupByInstallationStatus,
-  } = useFilterAndSort();
+  const { platformSortKey, platformSortDirection } = useFilterAndSort();
 
-  const path = useLocation({ select: (location) => location.pathname });
-  const { installations } = useInstallationIndex();
+  const { gameId } = useParams({ strict: false });
 
   const { data: platformData, status: platformStatus } = usePlatforms({
     request: { withMetadata: true },
   });
 
-  const { data: gameData, status: gameStatus } = useGames({
-    request: { withMetadata: true },
+  const { data: currentGame, status: currentGameStatus } = useGames({
+    request: { ids: gameId ? [parseInt(gameId)] : [-1] },
+    selectFn: (data) => data.games[0],
   });
 
-  const { data: allGameMetadata, status: gameMetadataStatus } = useGameMetadata(
-    {
-      request: { gameIds: gameData?.games.map((g) => g.id) },
-      selectFn: (data) =>
-        data.metadata.reduce(
-          (acc, value) => ({
-            ...acc,
-            [value.gameId]: {
-              metadata: value,
-              mediaPaths: data.mediaPaths[value.gameId],
-            },
-          }),
-          {} as Record<number, GameMetadataWithMediaPaths>,
-        ),
-    },
-  );
-
-  const currentGame = useMemo(() => {
-    try {
-      const id = parseInt(path.split("games/")[1]?.split("/")[0]);
-      return gameData?.games.find((game) => game.id === id);
-    } catch {
-      return undefined;
-    }
-  }, [path, gameData]);
-
   const loading =
-    platformStatus === "pending" ||
-    gameStatus === "pending" ||
-    gameMetadataStatus === "pending";
-
-  const error =
-    platformStatus === "error" ||
-    gameStatus === "error" ||
-    gameMetadataStatus === "error";
+    platformStatus === "pending" || currentGameStatus === "pending";
+  const error = platformStatus === "error" || currentGameStatus === "error";
 
   const platformsWithMetadata: PlatformWithMetadata[] = useMemo(() => {
     const platforms =
@@ -126,63 +52,6 @@ export function SideBar() {
       sortPlatforms(a, b, platformSortKey, platformSortDirection),
     );
   }, [platformData, platformSortKey, platformSortDirection]);
-
-  const gamesByPlatform = useMemo(() => {
-    if (!gameData || !platformData) return {};
-    const ret: Record<string, Array<Game & { metadata?: GameMetadata }>> = {};
-
-    const platforms = platformData.platforms;
-    const { games, metadata } = gameData;
-
-    const gamesWithMetadata = games
-      .map((game) => {
-        const gameMetadata = metadata.find((m) => m.gameId === game.id);
-        return {
-          ...game,
-          metadata: gameMetadata,
-        };
-      })
-      .filter((game) => filterName(game, filters.name));
-
-    return platforms.reduce((acc, platform) => {
-      const games = gamesWithMetadata.filter(
-        (game) => game.platformId === platform.id,
-      );
-
-      games.sort((a, b) => sortGames(a, b, gameSortKey, gameSortDirection));
-
-      if (groupByInstallationStatus) {
-        games.sort((a, b) => {
-          const aInstalled =
-            installations[a.id] === InstallationStatus.INSTALLED;
-
-          const bInstalled =
-            installations[b.id] === InstallationStatus.INSTALLED;
-
-          if (aInstalled && !bInstalled) {
-            return -1;
-          }
-
-          if (!aInstalled && bInstalled) {
-            return 1;
-          }
-
-          return 0;
-        });
-      }
-
-      acc[platform.id] = games;
-      return acc;
-    }, ret);
-  }, [
-    gameData,
-    platformData,
-    filters,
-    gameSortDirection,
-    gameSortKey,
-    groupByInstallationStatus,
-    installations,
-  ]);
 
   return (
     <div className="w-full h-full @container/sidebar">
@@ -214,39 +83,6 @@ export function SideBar() {
               Could not load game data 😔
             </p>
           </div>
-        ) : !platformsWithMetadata.some(
-            (p) => gamesByPlatform[p.id]?.length,
-          ) ? (
-          <div className="h-full grid place-items-center">
-            <div className="flex flex-col gap-4 text-center text-muted-foreground px-6">
-              <h3 className="text-2xl font-bold">Where all the games at?</h3>
-
-              <p>
-                Make sure Retrom knows about your{" "}
-                <Link
-                  className="text-accent-text"
-                  to="."
-                  search={(prev) => ({
-                    ...prev,
-                    configModal: { open: true, tab: "server" },
-                  })}
-                >
-                  library sources
-                </Link>{" "}
-                and then{" "}
-                <Link
-                  className="text-accent-text"
-                  to="."
-                  search={(prev) => ({
-                    ...prev,
-                    updateLibraryModal: { open: true },
-                  })}
-                >
-                  update your library
-                </Link>
-              </p>
-            </div>
-          </div>
         ) : (
           <ScrollArea
             className={cn(
@@ -264,246 +100,46 @@ export function SideBar() {
                 defaultValue={currentGame?.platformId?.toString()}
               >
                 {platformsWithMetadata.map((platform) => {
-                  const games = gamesByPlatform[platform.id] ?? [];
-
-                  const name =
-                    platform.metadata?.name || getFileStub(platform.path);
-
-                  if (platform.thirdParty && !games.length) {
-                    return null;
-                  }
-
-                  return (
-                    <AccordionItem
-                      key={platform.id}
-                      value={platform.id.toString()}
-                      className={cn("border-b-0 w-full max-w-full")}
-                    >
-                      <div
-                        className={cn(
-                          "group grid grid-cols-[1fr_auto] border-b border-transparent",
-                          "sm:hover:border-border transition-all",
-                        )}
-                      >
-                        <AccordionTrigger
-                          hideIcon
-                          className={cn(
-                            "group py-2 font-medium overflow-hidden relative hover:no-underline",
-                          )}
-                        >
-                          <div className="flex w-full">
-                            <h3 className="text-left text-lg sm:text-base whitespace-nowrap text-ellipsis overflow-hidden">
-                              {name}
-                            </h3>
-                            <span className="sr-only">Toggle</span>
-                          </div>
-                        </AccordionTrigger>
-                        <PlatformContextMenu platform={platform} />
-                      </div>
-
-                      <AccordionContent>
-                        {games.length ? (
-                          <ul>
-                            {games.map((game) => {
-                              const isCurrentGame = currentGame?.id === game.id;
-                              const installationStatus = installations[game.id];
-                              const isInstalled =
-                                installationStatus ===
-                                InstallationStatus.INSTALLED;
-
-                              const gameMetadata = allGameMetadata?.[game.id];
-
-                              const fallbackName =
-                                game.storageType ===
-                                StorageType.SINGLE_FILE_GAME
-                                  ? getFileStub(game.path)
-                                  : getFileName(game.path);
-
-                              const gameName =
-                                gameMetadata?.metadata?.name ?? fallbackName;
-
-                              return (
-                                <Tooltip key={game.id}>
-                                  <TooltipTrigger asChild>
-                                    <li
-                                      className={cn(
-                                        "relative z-10 before:z-[-1] before:duration-200",
-                                        "border-l border-border",
-                                        "before:absolute before:inset-0 before:transition-opacity",
-                                        "before:bg-gradient-to-r before:from-accent/40 before:opacity-0",
-                                        "text-[1rem] text-muted-foreground/40 transition-all",
-                                        !isCurrentGame &&
-                                          "sm:hover:before:opacity-60 sm:hover:text-primary-foreground/80",
-                                        isInstalled && "text-muted-foreground",
-                                        isCurrentGame &&
-                                          "before:opacity-100 text-primary-foreground border-accent border-l-4",
-                                        "max-w-full w-full overflow-hidden text-ellipsis px-2 py-0.5",
-                                      )}
-                                    >
-                                      <GameItem
-                                        game={game}
-                                        metadata={gameMetadata}
-                                        displayName={gameName}
-                                      />
-                                    </li>
-                                  </TooltipTrigger>
-                                  <TooltipPortal>
-                                    <TooltipContent
-                                      side="right"
-                                      className="hidden sm:block pointer-events-none touch-none"
-                                    >
-                                      {gameName}
-                                    </TooltipContent>
-                                  </TooltipPortal>
-                                </Tooltip>
-                              );
-                            })}
-                          </ul>
-                        ) : (
-                          <div className="grid place-items-center pt-4">
-                            <p className="font-medium text-muted-foreground italic">
-                              Theres no games here...
-                            </p>
-                          </div>
-                        )}
-                      </AccordionContent>
-                    </AccordionItem>
-                  );
+                  return <GameList key={platform.id} platform={platform} />;
                 })}
+
+                <div className="not-only:hidden py-10 grid place-items-center">
+                  <div className="flex flex-col gap-4 text-center text-muted-foreground px-6">
+                    <h3 className="text-2xl font-bold italic text-pretty">
+                      Where are all the games at?
+                    </h3>
+
+                    <p>
+                      Make sure Retrom knows about your{" "}
+                      <Link
+                        className="text-accent-text"
+                        to="."
+                        search={(prev) => ({
+                          ...prev,
+                          configModal: { open: true, tab: "server" },
+                        })}
+                      >
+                        library sources
+                      </Link>{" "}
+                      and then{" "}
+                      <Link
+                        className="text-accent-text"
+                        to="."
+                        search={(prev) => ({
+                          ...prev,
+                          updateLibraryModal: { open: true },
+                        })}
+                      >
+                        update your library
+                      </Link>
+                    </p>
+                  </div>
+                </div>
               </Accordion>
             </TooltipProvider>
           </ScrollArea>
         )}
       </aside>
     </div>
-  );
-}
-
-function GameItem(props: {
-  game: Game;
-  displayName: string;
-  metadata?: GameMetadataWithMediaPaths;
-}) {
-  const { game, displayName, metadata: data } = props;
-
-  const installationStatus = useInstallationStatus(game.id);
-  const { percentComplete } = useInstallationProgress(game.id);
-  const publicUrl = usePublicUrl();
-
-  const iconUrl = useMemo(() => {
-    const localPath = data?.mediaPaths?.iconUrl;
-    if (localPath && publicUrl) {
-      return createUrl({ path: localPath, base: publicUrl })?.href;
-    }
-
-    return data?.metadata?.iconUrl;
-  }, [data, publicUrl]);
-
-  const isInstalling =
-    installationStatus === InstallationStatus.INSTALLING ||
-    installationStatus === InstallationStatus.PAUSED;
-
-  return (
-    <Link
-      to="/games/$gameId"
-      params={{ gameId: game.id.toString() }}
-      className={cn("grid grid-cols-[auto_1fr] items-center max-w-full h-full")}
-    >
-      <div className="relative min-w-[28px] min-h-[28px] mr-2 my-[2px]">
-        {iconUrl && (
-          <Image src={iconUrl} width={28} height={28} alt={displayName} />
-        )}
-      </div>
-
-      <span
-        className={cn(
-          "whitespace-nowrap overflow-hidden text-ellipsis",
-          isInstalling && "text-primary",
-        )}
-      >
-        <span className="text-sm">
-          {isInstalling ? `${percentComplete}% - ` : ""}
-        </span>
-        <span className={cn(isInstalling && "animate-pulse")}>
-          {displayName}
-        </span>
-      </span>
-    </Link>
-  );
-}
-
-function PlatformContextMenu(
-  props: ComponentPropsWithoutRef<typeof DropdownMenuTrigger> & {
-    platform: PlatformWithMetadata;
-  },
-) {
-  const { platform, ...rest } = props;
-
-  const name = platform.metadata?.name || getFileStub(platform.path);
-  const { id, thirdParty } = platform;
-
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger
-        asChild
-        {...rest}
-        className={cn(
-          "sm:opacity-0 transition-opacity active:opacity-100",
-          "sm:group-hover:opacity-100 data-[state=open]:opacity-100",
-        )}
-      >
-        <Button
-          size="icon"
-          variant="ghost"
-          className="w-fit h-fit aspect-square p-2 my-auto"
-        >
-          <EllipsisVertical className={cn("w-[1rem] h-[1rem]")} />
-        </Button>
-      </DropdownMenuTrigger>
-
-      <DropdownMenuContent
-        className={cn(
-          "w-dvw flex flex-col items-center gap-2",
-          "sm:w-auto sm:block",
-        )}
-      >
-        <DropdownMenuItem
-          asChild
-          onClick={(e) => {
-            e.stopPropagation();
-          }}
-        >
-          <Link
-            to="."
-            search={(prev) => ({
-              ...prev,
-              updatePlatformMetadataModal: { open: true, id: platform.id },
-            })}
-          >
-            Edit
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          asChild
-          className="text-destructive-text"
-          onClick={(e) => {
-            e.stopPropagation();
-          }}
-        >
-          <Link
-            to="."
-            search={(prev) => ({
-              ...prev,
-              deletePlatformModal: {
-                open: true,
-                platform: { id, name, thirdParty },
-              },
-            })}
-          >
-            Delete
-          </Link>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
   );
 }
