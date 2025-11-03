91c14f07328a921f014d5981b72f1e29c6a23dcf
John Beresford <jberesford@volcaus.com>
2025-11-02 16:41:37 -0800

    feat: configure locally stored metadata quality

--- packages/client-web/src/components/modals/config/server/metadata-config.tsx
+++ packages/client-web/src/components/modals/config/server/metadata-config.tsx
@@ -5,36 +5,79 @@ import {
   FormControl,
   FormField,
   FormItem,
+  FormLabel,
   FormMessage,
 } from "@retrom/ui/components/form";
 import { TabsContent } from "@retrom/ui/components/tabs";
 import {
+  MetadataConfig_ImageFormat,
   MetadataConfigSchema,
   ServerConfig,
 } from "@retrom/codegen/retrom/server/config_pb";
 import { useUpdateServerConfig } from "@/mutations/useUpdateServerConfig";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useNavigate } from "@tanstack/react-router";
-import { Loader2Icon, LoaderCircleIcon, Trash2Icon } from "lucide-react";
+import {
+  InfoIcon,
+  Loader2Icon,
+  LoaderCircleIcon,
+  Sparkles,
+  Trash2Icon,
+} from "lucide-react";
 import { useCallback } from "react";
 import { useForm } from "@retrom/ui/components/form";
 import { z } from "zod";
-import { RawMessage } from "@/utils/protos";
 import { create } from "@bufbuild/protobuf";
 import { Checkbox } from "@retrom/ui/components/checkbox";
 import { cn } from "@retrom/ui/lib/utils";
 import { useServerLocalMetadataStatus } from "@/queries/useServerLocalMetadataStatus";
 import { useDeleteServerLocalMetadata } from "@/mutations/useDeleteServerLocalMetadata";
 import { getBestFileSizeOrder, readableByteSize } from "@/utils/files";
-
-type MetadataConfigShape = Record<
-  keyof NonNullable<RawMessage<ServerConfig["metadata"]>>,
-  z.ZodTypeAny
->;
+import { Input } from "@retrom/ui/components/input";
+import { Toggle } from "@retrom/ui/components/toggle";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@retrom/ui/components/tooltip";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@retrom/ui/components/select";
 
 const metadataSchema = z.object({
   storeMetadataLocally: z.boolean().default(false),
-}) satisfies z.ZodObject<MetadataConfigShape>;
+  optimization: z
+    .object({
+      jpegQuality: z.number().min(1).max(100).default(85),
+      jpegOptimization: z.boolean(),
+
+      pngOptimization: z.boolean(),
+      pngQuality: z.number().min(1).max(100).catch(85),
+      pngOptimizationLevel: z.number().min(0).max(6).catch(2),
+
+      webpQuality: z.number().min(1).max(100).catch(85),
+      webpLossless: z.boolean(),
+
+      preferredImageFormat: z
+        .nativeEnum(MetadataConfig_ImageFormat)
+        .catch(MetadataConfig_ImageFormat.JPEG),
+    })
+    .default({
+      jpegQuality: 85,
+      jpegOptimization: false,
+      pngOptimization: true,
+      pngQuality: 85,
+      pngOptimizationLevel: 2,
+      webpQuality: 85,
+      webpLossless: true,
+      preferredImageFormat: MetadataConfig_ImageFormat.JPEG,
+    }),
+});
 
 export function MetadataConfig(props: {
   currentConfig: NonNullable<ServerConfig>;
@@ -44,7 +87,9 @@ export function MetadataConfig(props: {
 
   const form = useForm<z.infer<typeof metadataSchema>>({
     resolver: zodResolver(metadataSchema),
-    defaultValues: create(MetadataConfigSchema, props.currentConfig.metadata),
+    defaultValues: metadataSchema.parse(
+      create(MetadataConfigSchema, props.currentConfig.metadata),
+    ),
   });
 
   const handleSubmit = useCallback(
@@ -70,70 +115,299 @@ export function MetadataConfig(props: {
   const canSubmit = dirty && status !== "pending";
 
   return (
-    <TabsContent value="metadata" className="mt-6 flex flex-col gap-4">
-      <LocalMetadataStatus />
+    <TooltipProvider>
+      <TabsContent value="metadata" className="mt-6 flex flex-col gap-6">
+        <LocalMetadataStatus />
 
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(handleSubmit)}
-          className="flex flex-col gap-4"
-        >
-          <FormField
-            control={form.control}
-            name="storeMetadataLocally"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <div className="flex items-top gap-2">
-                    <Checkbox
-                      disabled={status === "pending"}
-                      id="server-local-metadata-enabled"
-                      checked={field.value}
-                      onCheckedChange={(val) => {
-                        field.onChange(val);
-                      }}
-                    />
-                    <div className={cn("grid gap-1 leading-none")}>
-                      <label htmlFor="server-local-metadata-enabled">
-                        Enable Local Metadata Storage
-                      </label>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="flex flex-col gap-6"
+          >
+            <FormField
+              control={form.control}
+              name="storeMetadataLocally"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="flex items-top gap-2">
+                      <Checkbox
+                        disabled={status === "pending"}
+                        id="server-local-metadata-enabled"
+                        checked={field.value}
+                        onCheckedChange={(val) => {
+                          field.onChange(val);
+                        }}
+                      />
+                      <div className={cn("grid gap-1 leading-none")}>
+                        <label htmlFor="server-local-metadata-enabled">
+                          Enable Local Metadata Storage
+                        </label>
 
-                      <p className="text-sm text-muted-foreground">
-                        Fetch and store external metadata locally on the Retrom
-                        server.
-                      </p>
+                        <p className="text-sm text-muted-foreground">
+                          Fetch and store external metadata locally on the
+                          Retrom server.
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <DialogFooter className="gap-2">
-            <Button
-              type="button"
-              onClick={() =>
-                navigate({
-                  to: ".",
-                  search: (prev) => ({ ...prev, configModal: undefined }),
-                }).catch(console.error)
-              }
-              variant="secondary"
-            >
-              Close
-            </Button>
-            <Button type="submit" disabled={!canSubmit}>
-              {status === "pending" ? (
-                <LoaderCircleIcon className="animate-spin" />
-              ) : (
-                "Save"
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
               )}
-            </Button>
-          </DialogFooter>
-        </form>
-      </Form>
-    </TabsContent>
+            />
+
+            <FormField
+              control={form.control}
+              name="optimization.jpegQuality"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <FormLabel>JPEG Quality</FormLabel>
+                  <div className="flex items-center gap-4">
+                    <FormControl>
+                      <Input type="range" min={1} max={100} {...field} />
+                    </FormControl>
+                    <p className="text-sm font-semibold">{field.value}</p>
+                  </div>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              disabled
+              name="optimization.webpQuality"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    WebP Quality <em className="text-sm">(coming soon)</em>
+                  </FormLabel>
+                  <div className="flex items-center gap-4">
+                    <FormControl>
+                      <Input type="range" min={1} max={100} {...field} />
+                    </FormControl>
+                    <p className="text-sm font-semibold">{field.value}</p>
+                  </div>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              disabled
+              name="optimization.pngQuality"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    PNG Quality <em className="text-sm">(coming soon)</em>
+                  </FormLabel>
+                  <div className="flex items-center gap-4">
+                    <FormControl>
+                      <Input type="range" min={1} max={100} {...field} />
+                    </FormControl>
+                    <p className="text-sm font-semibold">{field.value}</p>
+                  </div>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              disabled
+              name="optimization.pngOptimizationLevel"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="flex items-center gap-1">
+                    <FormLabel>
+                      PNG Optimization Level{" "}
+                      <em className="text-sm">(coming soon)</em>
+                    </FormLabel>
+                    <Tooltip>
+                      <TooltipTrigger>
+                        <InfoIcon size={"1rem"} className="text-primary" />
+                      </TooltipTrigger>
+                      <TooltipContent className="max-w-sm">
+                        Lower levels are faster but less effective, while higher
+                        levels take more time but yield better compression.
+                      </TooltipContent>
+                    </Tooltip>
+                  </div>
+                  <div className="flex items-center gap-4">
+                    <FormControl>
+                      <Input type="range" min={0} max={6} {...field} />
+                    </FormControl>
+                    <p className="text-sm font-semibold">{field.value}</p>
+                  </div>
+                </FormItem>
+              )}
+            />
+
+            <div className="flex flex-col gap-4">
+              <div className="flex items-center gap-1">
+                <FormLabel>Lossless/Lossy Compression</FormLabel>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <InfoIcon size={"1rem"} className="text-primary" />
+                  </TooltipTrigger>
+                  <TooltipContent className="max-w-sm">
+                    Choose between lossless and lossy compression for each image
+                    format. Lossless compression retains all original image
+                    data, while lossy compression reduces file size by removing
+                    some data.
+                  </TooltipContent>
+                </Tooltip>
+              </div>
+
+              <div className="grid grid-cols-3 place-items-center bg-muted py-4">
+                <FormField
+                  control={form.control}
+                  name="optimization.jpegOptimization"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col justify-center gap-2">
+                      <p className="text-center">JPEG</p>
+                      <FormControl>
+                        <Toggle
+                          disabled={field.disabled}
+                          variant="outline"
+                          size="sm"
+                          pressed={field.value}
+                          onPressedChange={field.onChange}
+                        >
+                          <Sparkles />
+                          {field.value ? "Lossless" : "Lossy"}
+                        </Toggle>
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  disabled
+                  name="optimization.pngOptimization"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col justify-center gap-2">
+                      <p className="text-center">
+                        PNG <em className="text-xs">(coming soon)</em>
+                      </p>
+                      <FormControl>
+                        <Toggle
+                          disabled={field.disabled}
+                          variant="outline"
+                          size="sm"
+                          pressed={field.value}
+                          onPressedChange={field.onChange}
+                        >
+                          <Sparkles />
+                          {field.value ? "Lossless" : "Lossy"}
+                        </Toggle>
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  disabled
+                  name="optimization.webpLossless"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col justify-center gap-2">
+                      <p className="text-center">
+                        WEBP <em className="text-xs">(coming soon)</em>
+                      </p>
+                      <FormControl>
+                        <Toggle
+                          disabled={field.disabled}
+                          variant="outline"
+                          size="sm"
+                          pressed={field.value}
+                          onPressedChange={field.onChange}
+                        >
+                          <Sparkles />
+                          {field.value ? "Lossless" : "Lossy"}
+                        </Toggle>
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+
+            <FormField
+              control={form.control}
+              disabled
+              name="optimization.preferredImageFormat"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Preferred Image Format{" "}
+                    <em className="text-sm">(coming soon)</em>
+                  </FormLabel>
+                  <FormControl>
+                    <Select
+                      disabled={field.disabled}
+                      value={field.value?.toString()}
+                      onValueChange={(val) => field.onChange(parseInt(val))}
+                    >
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a format..." />
+                      </SelectTrigger>
+
+                      <SelectContent>
+                        <SelectItem
+                          value={MetadataConfig_ImageFormat.UNSPECIFIED.toString()}
+                        >
+                          None
+                        </SelectItem>
+
+                        <SelectItem
+                          value={MetadataConfig_ImageFormat.JPEG.toString()}
+                        >
+                          JPEG
+                        </SelectItem>
+
+                        <SelectItem
+                          value={MetadataConfig_ImageFormat.PNG.toString()}
+                        >
+                          PNG
+                        </SelectItem>
+
+                        <SelectItem
+                          value={MetadataConfig_ImageFormat.WEBP.toString()}
+                        >
+                          WEBP
+                        </SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <DialogFooter className="gap-2">
+              <Button
+                type="button"
+                onClick={() =>
+                  navigate({
+                    to: ".",
+                    search: (prev) => ({ ...prev, configModal: undefined }),
+                  }).catch(console.error)
+                }
+                variant="secondary"
+              >
+                Close
+              </Button>
+              <Button type="submit" disabled={!canSubmit}>
+                {status === "pending" ? (
+                  <LoaderCircleIcon className="animate-spin" />
+                ) : (
+                  "Save"
+                )}
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </TabsContent>
+    </TooltipProvider>
   );
 }
 
@@ -166,7 +440,7 @@ function LocalMetadataStatus() {
             {" "}
             {readableByteSize(
               totalByteSize,
-              getBestFileSizeOrder(totalByteSize / 100n),
+              getBestFileSizeOrder(totalByteSize / 10n),
             )}
           </span>
         </p>
