609cbd78223030ff0b7caf2fe34cc3874ca73d4e
John Beresford <jberesford@volcaus.com>
2025-12-20 23:42:50 -0800

    chore: update shadcn UI combobox styles

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -11,14 +11,15 @@ const buttonVariants = cva(
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+          "bg-destructive text-destructive-foreground hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
-          "border border-input shadow-xs bg-none hover:bg-accent hover:text-accent-foreground",
+          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-foreground/10 hover:text-accent-foreground",
+        ghost:
+          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         "ghost-outline":
-          "hover:bg-foreground/10 hover:text-accent-foreground border border-input shadow-xs",
+          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50 border border-input shadow-xs",
         inline: "p-0 h-min",
         link: "text-primary underline-offset-4 hover:underline",
         accent: "bg-accent text-accent-foreground hover:bg-accent/90",
