a5710e0b91b9f33d0a770280ee8b9abe334d6b57
John Beresford <jberesford@volcaus.com>
2026-01-10 18:03:57 -0800

    feat: init save sync support for desktop clients

--- packages/client-web/src/components/action-button/play-game-button.tsx
+++ packages/client-web/src/components/action-button/play-game-button.tsx
@@ -1,7 +1,10 @@
 import { Button } from "@retrom/ui/components/button";
 import { usePlayGame } from "@/mutations/usePlayGame";
 import { usePlayStatusQuery } from "@/queries/usePlayStatus";
-import { PlayStatus } from "@retrom/codegen/retrom/client/client-utils_pb";
+import {
+  PlayGamePayload,
+  PlayStatus,
+} from "@retrom/codegen/retrom/client/client-utils_pb";
 import { useStopGame } from "@/mutations/useStopGame";
 import {
   ComponentProps,
@@ -12,10 +15,22 @@ import {
 } from "react";
 import { LoaderCircleIcon, PlayIcon, PlusIcon, Square } from "lucide-react";
 import { useMatch, useNavigate } from "@tanstack/react-router";
-import { useToast } from "@retrom/ui/hooks/use-toast";
+import { toast } from "@retrom/ui/hooks/use-toast";
 import { Game } from "@retrom/codegen/retrom/models/games_pb";
 import { useDefaultEmulator } from "@/queries/useDefaultEmulator";
 import { useGameFiles } from "@/queries/useGameFiles";
+import { useMutation } from "@tanstack/react-query";
+import { checkIsDesktop } from "@/lib/env";
+import { match } from "ts-pattern";
+import {
+  SaveSyncStatus,
+  SyncBehavior,
+} from "@retrom/codegen/retrom/client/saves_pb";
+import { useModalAction } from "@/providers/modal-action";
+import { useSyncEmulatorSaves } from "@/mutations/useSyncEmulatorSaves";
+import { Emulator } from "@retrom/codegen/retrom/models/emulators_pb";
+import { Spinner } from "@retrom/ui/components/spinner";
+import { RawMessage } from "@/utils/protos";
 
 type PlayGameButtonProps = { game: Game } & ComponentProps<typeof Button>;
 
@@ -25,11 +40,73 @@ export const PlayGameButton = forwardRef(
     forwardedRef: ForwardedRef<HTMLButtonElement>,
   ) => {
     const { game } = props;
-    const { toast } = useToast();
+    const resolveSaveConflictModal = useModalAction("resolveCloudSaveConflict");
+    const { mutateAsync: syncEmulatorSaves } = useSyncEmulatorSaves();
     const { data: emulatorData } = useDefaultEmulator(game);
     const { data: gameFiles } = useGameFiles({
       request: { gameIds: [game.id] },
       selectFn: (data) => data.gameFiles.filter((f) => f.gameId === game.id),
+    });
+
+    const { mutateAsync: maybeSyncEmulatorSaves } = useMutation({
+      mutationFn: async (emulator: RawMessage<Emulator>) => {
+        const emulatorId = emulator.id;
+        const syncToast = toast({
+          title: `Syncing Saves: ${emulator.name}`,
+          duration: Infinity,
+          dismissible: false,
+          icon: <Spinner className="text-primary" />,
+        });
+
+        const response = await syncEmulatorSaves({
+          emulatorId,
+        });
+
+        if (
+          response.status === SaveSyncStatus.LOCAL_ERROR &&
+          response.conflictReport
+        ) {
+          return await new Promise((resolve, reject) => {
+            resolveSaveConflictModal.openModal({
+              status: response,
+              onClose: () => {
+                reject(new Error("Save conflict not resolved"));
+              },
+              onResolved: (choice) =>
+                match(choice)
+                  .with("local", () =>
+                    syncEmulatorSaves({
+                      emulatorId,
+                      behavior: SyncBehavior.FORCE_LOCAL,
+                    })
+                      .then((res) => resolve(res))
+                      .catch(reject),
+                  )
+                  .with("cloud", () =>
+                    syncEmulatorSaves({
+                      emulatorId,
+                      behavior: SyncBehavior.FORCE_CLOUD,
+                    })
+                      .then((res) => resolve(res))
+                      .catch(reject),
+                  )
+                  .with("skip", () => {
+                    resolve(null);
+                  })
+                  .exhaustive(),
+            });
+          });
+        }
+
+        syncToast.update({
+          title: `Saves synced: ${emulator.name}`,
+          icon: undefined,
+          dismissible: true,
+          duration: 5000,
+        });
+
+        return response;
+      },
     });
 
     const { mutate: playAction } = usePlayGame(game);
@@ -53,6 +130,38 @@ export const PlayGameButton = forwardRef(
     const disabled = queryStatus !== "success";
     const shouldAddEmulator = !emulator && !fullscreenMatch && !game.thirdParty;
 
+    const { mutate: playGame } = useMutation({
+      mutationFn: async (args: RawMessage<PlayGamePayload>) => {
+        const { emulator } = args;
+
+        if (checkIsDesktop() && emulator) {
+          try {
+            await maybeSyncEmulatorSaves(emulator);
+          } catch (error) {
+            const errorMsg =
+              error instanceof Error
+                ? error.message
+                : "An unknown error occurred.";
+
+            toast({
+              title: "Unable to Launch Game",
+              description: errorMsg,
+            });
+
+            return;
+          }
+        }
+
+        toast({
+          title: game.thirdParty ? "Launching External Game" : "Launching Game",
+          description: "Launching the game, this may take a few seconds.",
+          duration: 3000,
+        });
+
+        playAction(args);
+      },
+    });
+
     const onClick = useCallback(() => {
       if (disabled) return;
 
@@ -68,27 +177,20 @@ export const PlayGameButton = forwardRef(
         });
       }
 
-      toast({
-        title: game.thirdParty ? "Launching External Game" : "Launching Game",
-        description: "Launching the game, this may take a few seconds.",
-        duration: 3000,
-      });
-
-      playAction({
+      playGame({
         game,
         emulatorProfile: defaultProfile,
-        emulator: emulator,
+        emulator,
         file,
       });
     }, [
-      toast,
       navigate,
       disabled,
       defaultProfile,
       emulator,
       file,
       game,
-      playAction,
+      playGame,
       playStatusUpdate,
       stopAction,
       shouldAddEmulator,
--- packages/client-web/src/mutations/usePlayGame.ts
+++ packages/client-web/src/mutations/usePlayGame.ts
@@ -1,16 +1,15 @@
-import { GameSchema } from "@retrom/codegen/retrom/models/games_pb";
+import { Game } from "@retrom/codegen/retrom/models/games_pb";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { playGame } from "@retrom/plugin-launcher";
-import { PlayGamePayloadSchema } from "@retrom/codegen/retrom/client/client-utils_pb";
-import { MessageInitShape } from "@bufbuild/protobuf";
+import { PlayGamePayload } from "@retrom/codegen/retrom/client/client-utils_pb";
+import { RawMessage } from "@/utils/protos";
 
-export function usePlayGame(game?: MessageInitShape<typeof GameSchema>) {
+export function usePlayGame(game?: RawMessage<Game>) {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationKey: ["play", game?.id],
-    mutationFn: (payload: MessageInitShape<typeof PlayGamePayloadSchema>) =>
-      playGame(payload),
+    mutationFn: (payload: RawMessage<PlayGamePayload>) => playGame(payload),
     onError: console.error,
     onSuccess: () => {
       return queryClient.invalidateQueries({
