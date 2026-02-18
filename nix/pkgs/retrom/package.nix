38dc76790a8af35fe2815614d6dc6c6f6f92ee1c
Connor Davis <connor@scequ.com>
2026-02-18 16:14:38 +1100

    fix: nix support for client standalone
    uses FHS wrapper with library and share data dependecies required by
    embedded database

--- nix/pkgs/retrom/package.nix
+++ nix/pkgs/retrom/package.nix
@@ -2,24 +2,58 @@
   stdenv,
   lib,
   runCommand,
-  makeWrapper,
+  buildFHSEnv,
+
+  glibc,
+  zlib,
+  python311,
+  readline,
+  postgresql,
+  lz4,
+  krb5,
+  zstd,
+  openssl,
+  libxslt,
+  libxml2_13,
+  libossp_uuid,
+  tzdata,
 
   retrom-unwrapped,
 
   supportNvidia ? false,
 }:
-runCommand retrom-unwrapped.name
-  {
-    inherit (retrom-unwrapped) pname version meta;
+let
+  libossp_uuid' = stdenv.mkDerivation rec {
+    inherit (libossp_uuid) pname version meta;
+    src = libossp_uuid;
+    buildPhase = ''
+      mkdir -p $out/lib
+      ln -s ${libossp_uuid}/lib/libuuid.so.16 $out/lib/libossp-uuid.so.16
+    '';
+  };
+in
+buildFHSEnv {
+  inherit (retrom-unwrapped) pname version meta;
+  executableName = retrom-unwrapped.meta.mainProgram;
+  targetPkgs = pkgs: with pkgs; [
+    glibc
+    zlib
+    readline
+    python311
+    postgresql.lib
+    lz4.lib
+    krb5.lib
+    zstd.out
+    openssl.out
+    libxslt.out
+    libxml2_13.out
 
-    nativeBuildInputs = [
-      makeWrapper
-    ];
-  }
-  ''
-    mkdir -p $out/bin
-    ln -s ${retrom-unwrapped}/share $out/share
+    libossp_uuid'
 
-    makeWrapper ${retrom-unwrapped}/bin/Retrom $out/bin/Retrom \
-      ${lib.optionalString (supportNvidia && stdenv.isLinux) "--set WEBKIT_DISABLE_DMABUF_RENDERER 1"}
-  ''
+    tzdata
+  ];
+  runScript = lib.getExe retrom-unwrapped;
+  profile = ''
+    ${lib.optionalString (supportNvidia && stdenv.isLinux) "export WEBKIT_DISABLE_DMABUF_RENDERER=1"}
+  '';
+}
