0db85f9f7e9bfe75b81e7e8ddc2ef48f01f67ab1
John Beresford <jberesford@volcaus.com>
2026-03-28 11:14:29 -0700

    chore: add reflection svc

--- packages/service-jobs/src/main.rs
+++ packages/service-jobs/src/main.rs
@@ -1,3 +1,4 @@
+use retrom_codegen::descriptors::retrom::FILE_DESCRIPTOR_SET;
 use retrom_service_config::config::ServerConfigManager;
 use retrom_service_jobs::jobs_router;
 use retrom_telemetry::init_tracing_subscriber;
@@ -30,7 +31,26 @@ async fn main() {
 
     let addr: SocketAddr = format!("0.0.0.0:{port}").parse().unwrap();
 
-    let router = jobs_router().layer(tonic_web::GrpcWebLayer::new());
+    let reflection_service = tonic_reflection::server::Builder::configure()
+        .register_encoded_file_descriptor_set(FILE_DESCRIPTOR_SET)
+        .build_v1()
+        .unwrap();
+
+    let reflection_service_alpha = tonic_reflection::server::Builder::configure()
+        .register_encoded_file_descriptor_set(FILE_DESCRIPTOR_SET)
+        .build_v1alpha()
+        .unwrap();
+
+    let mut reflection_route_builder = tonic::service::Routes::builder();
+    reflection_route_builder
+        .add_service(reflection_service)
+        .add_service(reflection_service_alpha);
+
+    let reflection_router = reflection_route_builder.routes().into_axum_router();
+
+    let router = jobs_router()
+        .layer(tonic_web::GrpcWebLayer::new())
+        .merge(reflection_router.reset_fallback());
 
     let listener = tokio::net::TcpListener::bind(addr)
         .await
