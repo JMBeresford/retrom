93bc93f6f1ee198a49d65051ae95bd274a66661e
John Beresford <jberesford@volcaus.com>
2026-05-16 13:23:52 -0700

    fix(docker): remove legacy web server
    The Retrom API server has been serving the web client for a while now,
    however it seems the unused legacy web server was never removed. This fixes the
    docker image such that the legacy web server is now no longer started.

--- docker/start.sh
+++ docker/start.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
 function on_error() {
-    echo "Error in retrom startup script, line $1"
-    exit 1
+  echo "Error in retrom startup script, line $1"
+  exit 1
 }
 
 trap 'on_error $LINENO' ERR
-
-# Start the web server
-cd /app/web
-VITE_RETROM_LOCAL_SERVICE_HOST=http://localhost:5101 pnpm vite preview &
-web_pid=$!
 
 # Start the API server
 cd /app
@@ -19,9 +14,9 @@ cd /app
 api_pid=$!
 
 function kill_children() {
-    kill $web_pid $api_pid
-    wait $web_pid $api_pid
-    exit 0
+  kill $api_pid
+  wait $api_pid
+  exit 0
 }
 
 trap kill_children SIGINT SIGTERM
