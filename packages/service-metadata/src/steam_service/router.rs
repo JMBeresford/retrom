48de9db77123cfc5600dad38e125466133f4ce62
John Beresford <jberesford@volcaus.com>
2026-05-25 15:01:30 -0700

    chore(service-metadata): init service breakdown

---
+++ packages/service-metadata/src/steam_service/router.rs
@@ -0,0 +1,21 @@
+use crate::steam_service::SteamServiceHandlers;
+use retrom_codegen::retrom::services::metadata::v1::steam_service_server::SteamServiceServer;
+use retrom_db::DbPool;
+use retrom_service_config::config::ServerConfigManager;
+
+pub fn steam_router(db_pool: DbPool) -> axum::Router {
+    let config_manager =
+        Arc::new(ServerConfigManager::new().expect("Could not create config manager"));
+
+    let steam_provider = Arc::new(SteamWebApiProvider::new(config_manager.clone()));
+
+    let svc = SteamServiceServer::new(SteamServiceHandlers {
+        db_pool,
+        steam_provider,
+    });
+
+    let mut routes_builder = tonic::service::Routes::builder();
+    routes_builder.add_service(svc);
+
+    routes_builder.routes().into_axum_router().reset_fallback()
+}
