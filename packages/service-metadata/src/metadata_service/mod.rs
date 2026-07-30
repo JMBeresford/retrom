fc33661baaf2b57cf53168abfd554d290915c9a6
John Beresford <jberesford@volcaus.com>
2026-07-30 16:49:45 -0700

    chore: standardized metadata svc platform handlers

--- packages/service-metadata/src/metadata_service/mod.rs
+++ packages/service-metadata/src/metadata_service/mod.rs
@@ -4,6 +4,10 @@ use crate::metadata_service::game_metadata::{
     select_game_metadata_videos, select_similar_games, upsert_game_artworks, upsert_game_links,
     upsert_game_screenshots, upsert_game_videos, upsert_similar_games,
 };
+use crate::metadata_service::platform_metadata::{
+    insert_platform_metadata, platform_metadata_from_rows, rows_from_platform_metadata,
+    update_platform_metadata as update_platform_metadata_row,
+};
 use futures::{
     future::{join_all, try_join_all},
     FutureExt,
@@ -288,6 +292,82 @@ impl MetadataServiceHandlers {
             similar_game_rows,
         ))
     }
+
+    async fn handle_get_platform_metadata(
+        db_pool: DbPool,
+        request: GetPlatformMetadataRequest,
+    ) -> Result<PlatformMetadata, Status> {
+        let id = request.id;
+
+        let mut builder = QueryBuilder::new("select * from platform_metadata where id = ");
+        builder.push_bind(&id);
+        builder.push(" limit 1");
+
+        let row: retrom_codegen::retrom::services::metadata::v1::PlatformMetadataRow = builder
+            .build_query_as()
+            .fetch_one(&db_pool)
+            .await
+            .map_err(|e| Status::internal(e.to_string()))?;
+
+        Ok(platform_metadata_from_rows(row))
+    }
+
+    async fn handle_create_platform_metadata(
+        db_pool: DbPool,
+        request: CreatePlatformMetadataRequest,
+    ) -> Result<PlatformMetadata, Status> {
+        let metadata = match request.metadata {
+            Some(metadata) => metadata,
+            None => {
+                return Err(Status::invalid_argument(
+                    "metadata field is required for creating platform metadata".to_string(),
+                ));
+            }
+        };
+
+        if metadata.platform.trim().is_empty() {
+            return Err(Status::invalid_argument(
+                "platform field is required for creating platform metadata".to_string(),
+            ));
+        }
+
+        if metadata.provider.trim().is_empty() {
+            return Err(Status::invalid_argument(
+                "provider field is required for creating platform metadata".to_string(),
+            ));
+        }
+
+        let row = rows_from_platform_metadata(metadata);
+        let row = insert_platform_metadata(&db_pool, row).await?;
+
+        Ok(platform_metadata_from_rows(row))
+    }
+
+    async fn handle_update_platform_metadata(
+        db_pool: DbPool,
+        request: UpdatePlatformMetadataRequest,
+    ) -> Result<PlatformMetadata, Status> {
+        let field_mask: HashSet<String> = request
+            .update_mask
+            .unwrap_or_default()
+            .paths
+            .into_iter()
+            .collect();
+
+        let metadata = match request.metadata {
+            Some(metadata) => metadata,
+            None => {
+                return Err(Status::invalid_argument(
+                    "metadata field is required for updating platform metadata".to_string(),
+                ));
+            }
+        };
+
+        let row = rows_from_platform_metadata(metadata);
+        let row = update_platform_metadata_row(&db_pool, &row, &field_mask).await?;
+
+        Ok(platform_metadata_from_rows(row))
+    }
 }
 
 async fn spawn_cache_job<T, E, F>(job_manager: Arc<JobManager>, job_name: String, tasks: Vec<F>)
@@ -514,7 +594,10 @@ impl MetadataService for MetadataServiceHandlers {
         &self,
         request: Request<GetPlatformMetadataRequest>,
     ) -> Result<Response<PlatformMetadata>, Status> {
-        unimplemented!()
+        let metadata =
+            Self::handle_get_platform_metadata(self.db_pool.clone(), request.into_inner()).await?;
+
+        Ok(Response::new(metadata))
     }
 
     #[tracing::instrument(skip(self))]
@@ -522,7 +605,11 @@ impl MetadataService for MetadataServiceHandlers {
         &self,
         request: Request<CreatePlatformMetadataRequest>,
     ) -> Result<Response<PlatformMetadata>, Status> {
-        unimplemented!()
+        let metadata =
+            Self::handle_create_platform_metadata(self.db_pool.clone(), request.into_inner())
+                .await?;
+
+        Ok(Response::new(metadata))
     }
 
     #[tracing::instrument(skip(self))]
@@ -530,7 +617,56 @@ impl MetadataService for MetadataServiceHandlers {
         &self,
         request: Request<ListPlatformMetadataRequest>,
     ) -> Result<Response<ListPlatformMetadataResponse>, Status> {
-        unimplemented!()
+        let request = request.into_inner();
+        let ids = request.ids;
+        let platform_ids = request.platform_ids;
+        let provider_ids = request.provider_ids;
+
+        let mut builder =
+            QueryBuilder::new("select id from platform_metadata where id is not null ");
+
+        if !ids.is_empty() {
+            builder.push(" and id in (");
+            let mut separated = builder.separated(", ");
+            for id in ids {
+                separated.push_bind(id);
+            }
+            separated.push_unseparated(")");
+        }
+
+        if !platform_ids.is_empty() {
+            builder.push(" and platform_id in (");
+            let mut separated = builder.separated(", ");
+            for platform_id in platform_ids {
+                separated.push_bind(platform_id);
+            }
+            separated.push_unseparated(")");
+        }
+
+        if !provider_ids.is_empty() {
+            builder.push(" and provider_id in (");
+            let mut separated = builder.separated(", ");
+            for provider_id in provider_ids {
+                separated.push_bind(provider_id);
+            }
+            separated.push_unseparated(")");
+        }
+
+        let ids: Vec<String> = builder
+            .build_query_scalar()
+            .fetch_all(&self.db_pool)
+            .await
+            .map_err(|e| Status::internal(e.to_string()))?;
+
+        let metadata = try_join_all(ids.into_iter().map(|id| {
+            let db_pool = self.db_pool.clone();
+            async move {
+                Self::handle_get_platform_metadata(db_pool, GetPlatformMetadataRequest { id }).await
+            }
+        }))
+        .await?;
+
+        Ok(Response::new(ListPlatformMetadataResponse { metadata }))
     }
 
     #[tracing::instrument(skip(self))]
@@ -538,7 +674,50 @@ impl MetadataService for MetadataServiceHandlers {
         &self,
         request: Request<UpdatePlatformMetadataRequest>,
     ) -> Result<Response<PlatformMetadata>, Status> {
-        unimplemented!()
+        let request = request.into_inner();
+
+        let mut config_svc_client = self.config_client.clone();
+        let config = config_svc_client
+            .get_server_config(GetServerConfigRequest {})
+            .await?
+            .into_inner()
+            .config
+            .unwrap_or_default();
+
+        let store_metadata = config
+            .metadata
+            .map(|m| m.store_metadata_locally)
+            .unwrap_or(false);
+
+        if let Some(ref metadata) = request.metadata {
+            if let Err(e) = metadata.clean_cache().await {
+                error!("Failed to clean cache for metadata: {}", e);
+            }
+        }
+
+        if store_metadata {
+            if let Some(ref metadata) = request.metadata {
+                let metadata_config = self
+                    .config_client
+                    .clone()
+                    .get_server_config(GetServerConfigRequest {})
+                    .await?
+                    .into_inner()
+                    .config
+                    .and_then(|c| c.metadata);
+
+                self.cache_metadata(
+                    metadata,
+                    &format!("Cache Media Files For Platform {}", metadata.platform),
+                    metadata_config,
+                )
+                .await?;
+            }
+        };
+
+        let metadata = Self::handle_update_platform_metadata(self.db_pool.clone(), request).await?;
+
+        Ok(Response::new(metadata))
     }
 
     #[tracing::instrument(skip(self))]
@@ -546,7 +725,16 @@ impl MetadataService for MetadataServiceHandlers {
         &self,
         request: Request<BulkGetPlatformMetadataRequest>,
     ) -> Result<Response<BulkGetPlatformMetadataResponse>, Status> {
-        unimplemented!()
+        let requests = request.into_inner().requests;
+
+        let metadata = try_join_all(requests.into_iter().map(|r| {
+            let db_pool = self.db_pool.clone();
+
+            async move { Self::handle_get_platform_metadata(db_pool, r).await }
+        }))
+        .await?;
+
+        Ok(Response::new(BulkGetPlatformMetadataResponse { metadata }))
     }
 
     #[tracing::instrument(skip(self))]
@@ -554,7 +742,18 @@ impl MetadataService for MetadataServiceHandlers {
         &self,
         request: Request<BulkCreatePlatformMetadataRequest>,
     ) -> Result<Response<BulkCreatePlatformMetadataResponse>, Status> {
-        unimplemented!()
+        let requests = request.into_inner().requests;
+
+        let metadata = try_join_all(requests.into_iter().map(|r| {
+            let db_pool = self.db_pool.clone();
+
+            async move { Self::handle_create_platform_metadata(db_pool, r).await }
+        }))
+        .await?;
+
+        Ok(Response::new(BulkCreatePlatformMetadataResponse {
+            metadata,
+        }))
     }
 
     #[tracing::instrument(skip(self))]
@@ -736,10 +935,13 @@ impl MetadataService for MetadataServiceHandlers {
             .await?
             .into_inner();
 
-        self.update_platform_metadata(Request::new(UpdatePlatformMetadataRequest {
-            metadata: Some(igdb_metadata),
-            update_mask: None,
-        }))
+        Self::handle_update_platform_metadata(
+            self.db_pool.clone(),
+            UpdatePlatformMetadataRequest {
+                metadata: Some(igdb_metadata),
+                update_mask: None,
+            },
+        )
         .await?;
 
         Ok(Response::new(DownloadPlatformMetadataResponse {}))
--- packages/service-metadata/src/metadata_service/platform_metadata.rs
+++ packages/service-metadata/src/metadata_service/platform_metadata.rs
@@ -1,19 +1,133 @@
 use retrom_codegen::retrom::services::metadata::v1::{PlatformMetadata, PlatformMetadataRow};
 use retrom_db::RetromDB;
 use sqlx::{Executor, QueryBuilder};
+use std::collections::HashSet;
 use tonic::Status;
 
 pub fn platform_metadata_from_rows(row: PlatformMetadataRow) -> PlatformMetadata {
-    unimplemented!()
+    PlatformMetadata {
+        id: row.id,
+        platform: row.platform_id,
+        provider: row.provider_id,
+        provider_platform_id: row.provider_platform_id,
+        created_at: row.created_at,
+        updated_at: row.updated_at,
+        name: row.name,
+        description: row.description,
+        background_url: row.background_url,
+        icon_url: row.icon_url,
+        logo_url: row.logo_url,
+    }
 }
 
 pub fn rows_from_platform_metadata(metadata: PlatformMetadata) -> PlatformMetadataRow {
-    unimplemented!()
+    PlatformMetadataRow {
+        id: metadata.id,
+        platform_id: metadata.platform,
+        provider_id: metadata.provider,
+        provider_platform_id: metadata.provider_platform_id,
+        created_at: metadata.created_at,
+        updated_at: metadata.updated_at,
+        name: metadata.name,
+        description: metadata.description,
+        background_url: metadata.background_url,
+        icon_url: metadata.icon_url,
+        logo_url: metadata.logo_url,
+    }
 }
 
 pub async fn insert_platform_metadata(
     conn: impl Executor<'_, Database = RetromDB>,
     metadata: PlatformMetadataRow,
-) -> Result<(), Status> {
-    unimplemented!()
+) -> Result<PlatformMetadataRow, Status> {
+    let mut builder = QueryBuilder::new(
+        r#"
+        insert into platform_metadata (
+            id,
+            platform_id,
+            provider_id,
+            provider_platform_id,
+            name,
+            description,
+            background_url,
+            icon_url,
+            logo_url
+        )
+        values (
+        "#,
+    );
+
+    let mut separated = builder.separated(", ");
+    separated.push_bind(uuid::Uuid::now_v7().to_string());
+    separated.push_bind(&metadata.platform_id);
+    separated.push_bind(&metadata.provider_id);
+    separated.push_bind(&metadata.provider_platform_id);
+    separated.push_bind(&metadata.name);
+    separated.push_bind(&metadata.description);
+    separated.push_bind(&metadata.background_url);
+    separated.push_bind(&metadata.icon_url);
+    separated.push_bind(&metadata.logo_url);
+
+    builder.push(") returning *");
+
+    let row: PlatformMetadataRow = builder
+        .build_query_as()
+        .fetch_one(conn)
+        .await
+        .map_err(|e| Status::internal(e.to_string()))?;
+
+    Ok(row)
+}
+
+pub async fn update_platform_metadata(
+    conn: impl Executor<'_, Database = RetromDB>,
+    metadata: &PlatformMetadataRow,
+    field_mask: &HashSet<String>,
+) -> Result<PlatformMetadataRow, Status> {
+    let empty_mask = field_mask.is_empty();
+
+    let mut builder = QueryBuilder::new("update platform_metadata set ");
+    let mut separated = builder.separated(", ");
+
+    if empty_mask || field_mask.contains("name") {
+        separated
+            .push_unseparated("name = ")
+            .push_bind(&metadata.name);
+    }
+
+    if empty_mask || field_mask.contains("description") {
+        separated
+            .push_unseparated("description = ")
+            .push_bind(&metadata.description);
+    }
+
+    if empty_mask || field_mask.contains("background_url") {
+        separated
+            .push_unseparated("background_url = ")
+            .push_bind(&metadata.background_url);
+    }
+
+    if empty_mask || field_mask.contains("icon_url") {
+        separated
+            .push_unseparated("icon_url = ")
+            .push_bind(&metadata.icon_url);
+    }
+
+    if empty_mask || field_mask.contains("logo_url") {
+        separated
+            .push_unseparated("logo_url = ")
+            .push_bind(&metadata.logo_url);
+    }
+
+    builder.push(" where id = ");
+    builder.push_bind(&metadata.id);
+    builder.push(" returning *");
+
+    let row: PlatformMetadataRow = builder
+        .build_query_as()
+        .fetch_one(conn)
+        .await
+        .map_err(|e| Status::internal(e.to_string()))?;
+
+    Ok(row)
 }
